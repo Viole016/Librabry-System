2cedc89a63b701cbec65f44f5f20aaadae6dd69f
Kuroki <Kuroki@192.168.56.1>
2024-03-13 13:39:10 +0630

    first commit

---
+++ src/method/Calculation.java
@@ -0,0 +1,16 @@
+package method;
+
+public class Calculation {
+	int z;
+	public void addition(int x, int y) {
+		z = x + y;
+		System.out.println("The sum of given numbers: "+z);
+	}
+	
+	public void Subtraction(int x, int y) {
+		z = x - y;
+		System.out.println("The difference between given numbers: "+z);
+	}
+}
+
+	
